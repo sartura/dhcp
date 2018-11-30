48a90686c8d11f7f5b3f5523af6eacbe6ac8ac89
Mislav Novakovic <mislav.novakovic@sartura.hr>
2018-11-30 18:59:41 +0100

    update logic for SYSREPO_LESS_0.7.7
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- src/dhcp.c
+++ src/dhcp.c
@@ -52,12 +52,10 @@ sr_uci_mapping_t table_sr_uci[] = {
 static int
 #ifdef SYSREPO_LESS_0_7_5
 dhcp_v4_state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, void *private_ctx)
-#else
-#ifdef SYSREPO_LESS_0_7_7
+#elif SYSREPO_LESS_0_7_7
 dhcp_v4_state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, uint64_t request_id, void *private_ctx)
 #else
 dhcp_v4_state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, uint64_t request_id, const char *original_xpath, void *private_ctx)
-#endif
 #endif
 {
     int rc = SR_ERR_OK;
@@ -77,12 +75,10 @@ error:
 static int
 #ifdef SYSREPO_LESS_0_7_5
 dhcp_v6_state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, void *private_ctx)
-#else
-#ifdef SYSREPO_LESS_0_7_7
+#elif SYSREPO_LESS_0_7_7
 dhcp_v6_state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, uint64_t request_id, void *private_ctx)
 #else
 dhcp_v6_state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, uint64_t request_id, const char *original_xpath, void *private_ctx)
-#endif
 #endif
 {
     int rc = SR_ERR_OK;
